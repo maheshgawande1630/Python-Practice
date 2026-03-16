4a7272a23ede5c443fadd275e8e71c5007208186
Mahesh Gawande <maheshgawande681@gmail.com>
2026-03-16 10:30:27 +0530

    if elpython -u c:\Users\Mahesh\OneDrive\Coding\Python\Python-Practice\lecture-2\conditional_statement.py

---
+++ lecture-2/conditional_statement.py
@@ -0,0 +1,19 @@
+"""  Q. Grade studens based on marks
+
+    marks>=90,grade="A"
+    90>marks>=80,grade="B"
+    80>marks>=70,grade="c"
+    70>marks,grade="D"      """
+
+
+marks=float(input("Enter marks: "))
+
+if(marks>=90):
+        print("Grade=A")
+elif(90>marks>=80):
+        print("Grade=B")
+elif(80>marks>70):
+        print("Grade=C")
+elif(70>marks):
+        print("Grade=D")    
+                    
