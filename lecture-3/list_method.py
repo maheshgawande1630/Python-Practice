ea8bf077485193a9cdf03f08aa0efead0513fade
Mahesh Gawande <maheshgawande681@gmail.com>
2026-03-18 12:52:14 +0530

    list methods in py

---
+++ lecture-3/list_method.py
@@ -0,0 +1,11 @@
+
+list=[3,4,8,]
+
+list.append(5) #add element to the end
+list.sort() #sort in ascending order
+list.sort(reverse=True) #sort in decending order
+list.reverse() #reverses list
+list.insert(1,6)
+list.remove(4)
+list.pop(2)
+print(list)
---
+++ lecture-3/list_slicing.py
@@ -0,0 +1,5 @@
+# slicing in py
+
+marks=[90.4, 89.4 , 95.3, 80.3, 78.3]
+
+print(marks[1:4])
